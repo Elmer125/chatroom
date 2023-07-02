5eec5a2ba1aaaf53b273ac4863397cb7dfe87793
Elmer125 <elmermauricio05@hotmail.com>
2023-07-02 09:11:33 -0500

    Modifies devise controllers, Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "elmer",
+            email: "elmer@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+User.create(username: "mauricio",
+            email: "mauricio@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+User.create(username: "test",
+            email: "test@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+#Create messages
+50.times do |i|
+  Message.create(
+    body: Faker::Lorem.sentence(word_count: 10),
+    user_id: rand(1..3)
+  )
+end
+
